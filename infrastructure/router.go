3ac1e437eb79152656d8977d590102df3269a384
po3rin <abctail30@gmail.com>
2019-01-31 18:31:50 +0900

    separate env

--- infrastructure/router.go
+++ infrastructure/router.go
@@ -1,9 +1,6 @@
 package infrastructure
 
 import (
-	"net/http"
-	"os"
-
 	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
 	"github.com/po3rin/github_link_creator/handler"
@@ -27,10 +24,5 @@ func (r *Router) InitRouter() *gin.Engine {
 	router.Use(static.Serve("/", static.LocalFile("client/dist", true)))
 	router.GET("/api/v1/health", r.Handler.HealthCheck)
 	router.GET("/api/v1/images/:user/:repo", r.Handler.GetCode)
-	router.GET("/api/v1/fdceda", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"val": os.Getenv("GITHUB_CLIENT_ID"),
-		})
-	})
 	return router
 }
