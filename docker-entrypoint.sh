33483614c1b6430a0cc7701cab6a1d23d4ed3864
po3rin <abctail30@gmail.com>
2019-01-31 16:40:31 +0900

    separate env

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
-export GITHUB_CLIRNT_ID=$(aws ssm get-parameters --name ${PARAMETER_STORE_PREFIX}/clientid --query "Parameters[0].Value" --region ap-northeast-1 --output text)
-export GITHUB_SECRET=$(aws ssm get-parameters --name ${PARAMETER_STORE_PREFIX}/clientsecret --with-decryption --query "Parameters[0].Value" --region ap-northeast-1 --output text)
+# export GITHUB_CLIRNT_ID=$(aws ssm get-parameters --name ${PARAMETER_STORE_PREFIX}/clientid --query "Parameters[0].Value" --region ap-northeast-1 --output text)
+# export GITHUB_SECRETGITHUB_SECRET=$(aws ssm get-parameters --name ${PARAMETER_STORE_PREFIX}/clientsecret --with-decryption --query "Parameters[0].Value" --region ap-northeast-1 --output text)
+# env
+
+export GITHUB_CLIRNT_ID=$(aws ssm get-parameters --name /ghlinkcard/github/clientid --query "Parameters[0].Value" --region ap-northeast-1 --output text)
+export GITHUB_SECRETGITHUB_SECRET=$(aws ssm get-parameters --name /ghlinkcard/github/clientsecret --with-decryption --query "Parameters[0].Value" --region ap-northeast-1 --output text)
+env
